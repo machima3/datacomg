3f2d1cb4427b0d08dbcea9e2272302cef21bbcfc
Miguel Angel Chica Martínez <machima@datacom3.oficina>
2018-03-01 15:54:10 +0100

    add users

--- skills/command-help.js
+++ skills/command-help.js
@@ -8,6 +8,7 @@ module.exports = function (controller) {
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "espacio") + ": copia el espacio";
+        text += "\n- " + bot.enrichCommand(message, "añadir") + ": añade un participante";
         bot.reply(message, text);
     });
 }
